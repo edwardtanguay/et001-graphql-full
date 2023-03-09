8f5697c16f0209b68e9301a81baf2b376ace159d
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-09 15:50:18 +0100

    commented out

--- src/schema.ts
+++ src/schema.ts
@@ -1,27 +1,29 @@
 import { buildSchema } from 'graphql';
 
-export const schema = buildSchema(`
-        type Query {
-            message: String,
-            departments: [String]
-            jobs: [Job],
-            skills: [Skill]
-        }
 
-        type Job {
-            id: Int,
-            title: String, 
-            company: String,
-            url: String,
-            description: String,
-            skillList: String,
-            publicationDate: String
-        }
 
-        type Skill {
-            idCode: String,
-            name: String,
-            url: String,
-            description: String
-        }
-    `);
+// export const schema = buildSchema(`
+//         type Query {
+//             message: String,
+//             departments: [String]
+//             jobs: [Job],
+//             skills: [Skill]
+//         }
+
+//         type Job {
+//             id: Int,
+//             title: String,
+//             company: String,
+//             url: String,
+//             description: String,
+//             skillList: String,
+//             publicationDate: String
+//         }
+
+//         type Skill {
+//             idCode: String,
+//             name: String,
+//             url: String,
+//             description: String
+//         }
+//     `);
